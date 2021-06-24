83ca8839fa6a4ab7769127fa1a95ffcdaf7c030a
Omar Garcia <isc.omargarcia@gmail.com>
2021-06-24 02:42:14 -0500

    Update YamlRepository

--- scripts/models/repositories.py
+++ scripts/models/repositories.py
@@ -1,16 +1,18 @@
-import yaml
-
 class YamlRepository():
-    def __init__(self, file_path):
+    import yaml
+    import os
+
+    def __init__(self, file_name):
+        file_path = os.path.join(os.path.dirname(__file__), 'data', file_name)
         with open(file_path, 'r') as file:
             self.source = yaml.safe_load(file)
 
 class ZoneRepository(YamlRepository):
     def __init__(self):
-        super().__init__('../data/zones.yml')
+        super().__init__('zones.yml')
         self.zones = self.source.get('zones')
 
 class ActionRepository(YamlRepository):
     def __init__(self) -> None:
-        super().__init__('../data/actions.yml')
+        super().__init__('actions.yml')
         self.actions = self.source.get('actions')
