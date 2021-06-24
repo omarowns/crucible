ae055963d117975fdc93cc1aadb7c5d65f2d1c7d
Omar Garcia <isc.omargarcia@gmail.com>
2021-06-24 15:28:23 -0500

    fixes

--- scripts/animations/compounds.py
+++ scripts/animations/compounds.py
@@ -8,11 +8,11 @@ from effect_queue import EffectQueue
 class SirenAnimation(SegmentableAnimation):
 
     def __init__(self, args={}):
+        import pdb; pdb.set_trace()
         super().__init__(args=args)
         
         self.first_zone = Zone(start=self.range[0], end=int(self.range[-1]/2))
         self.last_zone = Zone(start=int(self.range[-1]/2), end=self.range[-1])
-        import pdb; pdb.set_trace()
         self.siren_one_effect = self.siren_effect_for(args.get("siren_one"))
         self.siren_two_effect = self.siren_effect_for(args.get("siren_two"))
         self.clear_effect = Effect(name="ClearAnimation")
