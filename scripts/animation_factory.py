0ab91887e80819e0182e1c7a5ff1fe6d83257a3c
Omar Garcia <isc.omargarcia@gmail.com>
2021-06-23 19:59:33 -0500

    Added a bit logging

--- scripts/animation_factory.py
+++ scripts/animation_factory.py
@@ -1,3 +1,4 @@
+import logging
 from strip import Strip
 from default_animations import *
 from rpi_ws281x import Color
@@ -10,6 +11,8 @@ class AnimationFactory():
 
     def  __init__(self):
         self.strip = Strip()
+        logging.debug("Strip initialized")
 
     def render(self, renderer_class, rest = None):
+        logging.debug(f'Invoking {renderer_class} with args {rest}')
         self._AVAILABLE_FACTORIES[renderer_class](self.strip, args = rest).render
