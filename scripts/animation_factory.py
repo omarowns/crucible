0f53f77b6cdcc4ea4644d82b88855141f982b40b
Omar Garcia <isc.omargarcia@gmail.com>
2021-06-23 20:53:37 -0500

    possible fix

--- scripts/animation_factory.py
+++ scripts/animation_factory.py
@@ -11,5 +11,5 @@ class AnimationFactory():
     def  __init__(self):
         self.strip = Strip()
 
-    def render(self, renderer_class, rest = None):
+    def render(self, renderer_class, rest = {}):
         self._AVAILABLE_FACTORIES[renderer_class](self.strip, args = rest).render()
