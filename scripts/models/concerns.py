f1cb947de8014bb74a59cf0dfe5fd928c552cb77
Omar Garcia <isc.omargarcia@gmail.com>
2021-06-24 13:51:22 -0500

    update concerns

--- scripts/models/concerns.py
+++ scripts/models/concerns.py
@@ -7,33 +7,29 @@ class Animatable():
         self.animation_classes = []
         self.animation_class = None
         self.animation = None
-        import pdb; pdb.set_trace()
         self.load_animation_classes()
 
     def animation_for(self, name):
         if self.animation_class != None:
             return self.animation_class
-
-        animation_classes = list(filter(lambda klass : str(klass) == name, self.animation_classes))
         
-        if len(animation_classes) == 1:
-            self.animation_class = animation_classes[0]
+        self.load_animation_class(name)
 
-        self.animation_class
-
-    def load_animation_class(self):
+    def load_animation_class(self, name):
         self.animation_class = None
 
-        effect_animation_classes = list(filter(lambda klass : str(klass) == self.name, self.animation_classes))
-        if len(effect_animation_classes) == 1:
-            self.animation_class = effect_animation_classes[0]
+        for klass in self.animation_classes:
+            if name == klass.__name__:
+                self.animation_class = klass
+
+        self.animation_class
 
     def load_animation_classes(self):
         try:
             import_module('animations.basics')
             import_module('animations.compounds')
             
-            self.animation_classes = {}
+            self.animation_classes = set([])
 
             for (klass, _) in implementations(getattr(import_module('animations'), 'basics')):
                 self.animation_classes.add(getattr(import_module('animations.basics'), klass))
@@ -41,7 +37,7 @@ class Animatable():
             for (klass, _) in implementations(getattr(import_module('animations'), 'compounds')):
                 self.animation_classes.add(getattr(import_module('animations.compunds'), klass))
 
-            self.animation_classes = list(self.animation_classes)
+            self.animation_classes = [*self.animation_classes]
         except:
             self.animation_classes = []
 
@@ -56,7 +52,8 @@ class StageableAnimation(Animatable):
 
     def stage_animation_with_zone(self, zone=Zone, arguments = {}):
         if self.animation_class == None:
-            return
+            if not self.load_animation_class():
+                return
 
         self.animation = self.animation_class({ "range": zone.range(), **arguments})
 
