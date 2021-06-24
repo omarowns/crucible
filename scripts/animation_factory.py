d665e6cc297d6a702270a871520c75eca7cb794b
Omar Garcia <isc.omargarcia@gmail.com>
2021-06-23 19:54:22 -0500

    fix string class thing

--- scripts/animation_factory.py
+++ scripts/animation_factory.py
@@ -3,8 +3,13 @@ from default_animations import *
 from rpi_ws281x import Color
 
 class AnimationFactory():
+    _AVAILABLE_FACTORIES = {
+        "ClearAnimation": ClearAnimation,
+        "StaticAnimation": StaticAnimation
+    }
+
     def  __init__(self):
         self.strip = Strip()
 
     def render(self, renderer_class, rest = None):
-        renderer_class(self.strip, args = rest).render
+        self._AVAILABLE_FACTORIES[renderer_class](self.strip, args = rest).render
