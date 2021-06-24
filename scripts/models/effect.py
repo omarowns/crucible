b950d86372d923cada189b72236b632bc0c9dfc3
Omar Garcia <isc.omargarcia@gmail.com>
2021-06-24 12:47:42 -0500

    More refactors

--- scripts/models/effect.py
+++ scripts/models/effect.py
@@ -1,9 +1,9 @@
 from models.zone import Zone
-from models.concerns import RenderableAnimation
+from models.concerns import Animatable, StageableAnimation, RenderableAnimation
 # import animations.basics
 # import animations.compounds
 
-class Effect(RenderableAnimation):
+class Effect(RenderableAnimation, StageableAnimation, Animatable):
     def __init__(self, name=None, arguments={}):
         super().__init__()
         self.id = id
