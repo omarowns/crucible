f88257ca64045c09f94fed40894fcc9641357eae
Omar Garcia <isc.omargarcia@gmail.com>
2021-06-24 15:14:21 -0500

    fixes

--- scripts/animations/compounds.py
+++ scripts/animations/compounds.py
@@ -13,7 +13,7 @@ class SirenAnimation(StaticAnimation):
         
         self.first_zone = Zone(start=self.range[0], end=int(self.range[-1]/2))
         self.last_zone = Zone(start=int(self.range[-1]/2), end=self.range[-1])
-
+        import pdb; pdb.set_trace()
         self.siren_one_effect = self.siren_effect_for(args.get("siren_one"))
         self.siren_two_effect = self.siren_effect_for(args.get("siren_two"))
         self.clear_effect = Effect(name="ClearAnimation")
