75c3690eb371f3f6f14ce7f9970d2814069feea7
Omar Garcia <isc.omargarcia@gmail.com>
2021-06-25 18:53:20 -0500

    fixes

--- scripts/models/effect.py
+++ scripts/models/effect.py
@@ -5,7 +5,7 @@ from models.concerns import RenderableAnimation
 
 class Effect(RenderableAnimation):
     def __init__(self, name=None, arguments={}):
-        super().__init__(arguments=arguments)
+        super().__init__()
         self.id = id
         self.name = name
         self.arguments = arguments
